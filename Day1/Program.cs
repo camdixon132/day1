3e99396f914d410b479404d24004ec527bccfe8c
camdixon132 <camdixon132@gmail.com>
2023-01-09 10:45:25 -0500

    first commit

---
+++ Day1/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//example
+
+//NULL
+string myName = null;
+Console.WriteLine(myName);
+string friend = "Josh Carolin";
+
+if (friend == "Family")
+{
+    myName = "Justin";
+}
+else if (friend == "Josh Carolin")
+{
+    myName = "Jeo";
+}
+Console.WriteLine(myName);
